f1baa49b76d089833b0d62775613f4c1a6472352
Vladimir Katasonov <vkatason@student.42malaga.com>
2024-06-19 16:19:04 +0200

    ex01 structure added

---
+++ Module_00/ex01/includes/Contact.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 14:31:43 by vkatason          #+#    #+#             */
+/*   Updated: 2024/06/19 15:45:07 by vkatason         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <string>
+
+class Contact
+{
+	private:
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickName;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
+	
+	public:
+		// Empty constructor
+		Contact()
+		{};
+		
+		// Empty destructor
+		~Contact()
+		{};
+		
+		//Getters
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getNickName() const;
+		std::string getPhoneNumber() const;
+		std::string getDarkestSecret() const;
+		
+		//Setters
+		void setFirstName(std::string firstName);
+		void setLastName(std::string lastName);
+		void setNickName(std::string nickName);
+		void setPhoneNumber(std::string phoneNumber);
+		void setDarkestSecret(std::string darkestSecret);
+};
+
+#endif
