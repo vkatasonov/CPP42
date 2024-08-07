b350de2f401b7c69ae139e61a1fed761d7a1de1f
vkatasonov <vladimir.katasonov@gmail.com>
2024-08-07 05:19:12 +0200

    Orthodox Class Implementation

--- Module_02/ex00/Fixed.cpp
+++ Module_02/ex00/Fixed.cpp
@@ -6,7 +6,45 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 18:27:06 by vkatason          #+#    #+#             */
-/*   Updated: 2024/07/29 18:27:10 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/08/07 05:15:50 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+	: _raw_bits(0)
+{
+	std::cout << BG_GREEN << "Default constructor called" << RST << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << BG_BLUE << "Copy constructor called" << RST << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+	std::cout << BG_ORANGE << "Copy assignment operator called" << RST << std::endl;
+	if (this != &src)
+		this->_raw_bits = src.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << BG_RED << "Destructor called" << RST << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << BG_YELLOW << "getRawBits member function called" << RST << std::endl;
+	return (this->_raw_bits);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << BG_YELLOW << "setRawBits member function called" << RST << std::endl;
+	this->_raw_bits = raw;
+}
--- Module_02/ex00/main.cpp
+++ Module_02/ex00/main.cpp
@@ -6,7 +6,19 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 18:27:41 by vkatason          #+#    #+#             */
-/*   Updated: 2024/07/29 18:27:48 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/08/07 03:49:39 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+int main(void)
+{
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
