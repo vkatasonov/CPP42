dc6966f67c6044ef51cd544d6c560b9ef633f91e
Vladimir Katasonov <vkatason@student.42malaga.com>
2024-09-16 15:12:13 +0200

    Module_07 | ex00 implemented

--- Module_07/ex00/main.cpp
+++ Module_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 10:02:23 by vkatason          #+#    #+#             */
-/*   Updated: 2024/09/15 19:04:18 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/09/16 15:10:59 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,53 @@
 
 int	main(void)
 {
-	int	a;
-	int	b;
-
-	a = 2;
-	b = 3;
-	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	int	a = 2;
+	int	b = 3;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	float e = 42.42f;
+	float f = 13.37f;
+	double g = 3.14;
+	double h = 2.718;
+	
+	std::cout << YELLOW << "************ Initial values ***********" << RST << std::endl;
+	std::cout << BLUE << "a = " << RST << a << std::endl;
+	std::cout << BLUE << "b = " << RST << b << std::endl;
+	std::cout << BLUE << "c = " << RST << c << std::endl;
+	std::cout << BLUE << "d = " << RST << d << std::endl;
+	std::cout << BLUE << "e = " << RST << e << std::endl;
+	std::cout << BLUE << "f = " << RST << f << std::endl;
+	std::cout << BLUE << "g = " << RST << g << std::endl;
+	std::cout << BLUE << "h = " << RST << h << std::endl;
+	
+	std::cout << YELLOW << "************ Test with int ************" << RST << std::endl;
+	::swap(a, b);
+	std::cout << BLUE << "Swap a and b" << RST << std::endl;
+	std::cout << BLUE << "a = " << RST << a << BLUE << "\nb = " << RST << b << std::endl;
+	std::cout << BLUE << "min( a, b ) = " << RST << ::min(a, b) << std::endl;
+	std::cout << BLUE << "max( a, b ) = " << RST << ::max(a, b) << std::endl;
+	
+	std::cout << YELLOW << "*********** Test with string **********" << RST << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	std::cout << BLUE << "Swap c and d" << RST << std::endl;
+	std::cout << BLUE << "c = " << RST << c << BLUE << "\nd = " << RST << d << std::endl;
+	std::cout << BLUE "min( c, d ) = " << RST << ::min(c, d) << std::endl;
+	std::cout << BLUE "max( c, d ) = " << RST << ::max(c, d) << std::endl;
+
+	std::cout << YELLOW << "*********** Test with float ***********" << RST << std::endl;
+	::swap(e, f);
+	std::cout << BLUE << "Swap e and f" << RST << std::endl;
+	std::cout << BLUE << "e = " << RST << e << BLUE << "\nf = " << RST << f << std::endl;
+	std::cout << BLUE << "min( e, f ) = " << RST << ::min(e, f) << std::endl;
+	std::cout << BLUE << "max( e, f ) = " << RST << ::max(e, f) << std::endl;
+	
+	std::cout << YELLOW << "********** Test with double ***********" << RST << std::endl;
+	::swap(g, h);
+	std::cout << BLUE << "Swap g and h" << RST << std::endl;
+	std::cout << BLUE << "g = " << RST << g << BLUE << "\nh = " << RST << h << std::endl;
+	std::cout << BLUE << "min( g, h ) = " << RST << ::min(g, h) << std::endl;
+	std::cout << BLUE << "max( g, h ) = " << RST << ::max(g, h) << std::endl;
+	std::cout << YELLOW << "***************************************" << RST << std::endl;
+	
 	return (0);
 }
--- Module_07/ex00/whatever.hpp
+++ Module_07/ex00/whatever.hpp
@@ -6,9 +6,61 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 10:05:01 by vkatason          #+#    #+#             */
-/*   Updated: 2024/09/15 19:05:23 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/09/16 14:43:53 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <cmath>
+#include <limits>
+
+template <typename T> 
+void swap(T &a, T &b)
+{
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T> 
+const T &min(const T &a, const T &b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T> 
+const T &max(const T &a, const T &b)
+{
+	return (a > b ? a : b);
+}
+
+/*
+** ASCII colors escape sequences for printf
+*/
+#define RST "\033[0m"					            /* Reset to default color */
+#define BU "\033[1;4;0m"				            /* Bold and underlined text*/
+#define BOLD "\033[1m"						            /* Bold text default color */
+#define U "\033[4m"						            /* Underlined text default color */
+#define RED "\033[1;31m"				            /* Bold Red*/
+#define GREEN "\033[1;32m"				            /* Bold Green */
+#define YELLOW "\033[1;33m"				            /* Bold Yellow */
+#define BLUE "\033[1;34m"				            /* Bold Blue */
+#define MAGENTA "\033[1;35m"			            /* Bold Magenta */
+#define CYAN "\033[1;36m"				            /* Bold Cyan */
+#define WHITE "\033[1;37m"				            /* Bold White */
+#define ORANGE "\033[1m\033[38;2;255;165;0m"	    /* Orange */
+#define VIOLET "\033[1m\033[38;2;238;130;238m"	    /* Violet */
+#define BG_BLACK "\033[40m"				            /* Background Black */
+#define BG_GREEN "\033[42m"				            /* Background Green */
+#define BG_YELLOW "\033[43m"			            /* Background Yellow */
+#define BG_RED "\033[41m"				            /* Background Red */
+#define BG_BLUE "\033[44m"				            /* Background Blue */
+#define BG_MAGENTA "\033[45m"			            /* Background Magenta */
+#define BG_CYAN "\033[46m"				            /* Background Cyan */
+#define BG_WHITE "\033[47m"				            /* Background White */
+#define BG_ORANGE "\033[48;2;255;165;0m"            /* Background Orange */
+#define BG_VIOLET "\033[48;2;238;130;238m"          /* Background Violet */
+#define BG_RESET "\033[49m"				            /* Background Reset */
