1b0243358f7e8da3dadefe12e5eb7c93180bf937
Vladimir Katasonov <vkatason@student.42malaga.com>
2024-09-19 15:10:57 +0200

    Module 08 | ex 00 (easyfinder) implemented

--- Module_08/ex00/easyfind.tpp
+++ Module_08/ex00/easyfind.tpp
@@ -5,8 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/15 15:18:22 by vkatason          #+#    #+#             */
-/*   Updated: 2024/09/17 15:18:28 by vkatason         ###   ########.fr       */
+/*   Created: 2024/09/15 14:22:23 by vkatason          #+#    #+#             */
+/*   Updated: 2024/09/19 14:45:12 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+const char* NotFoundException::what() const throw()
+{
+    return "Element not found";
+}
+
+template <class T>
+typename T::iterator easyfind(T& container, int toFind)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), toFind);
+    if (it != container.end())
+        return it;
+    else
+        throw NotFoundException();
+}
--- Module_08/ex00/main.cpp
+++ Module_08/ex00/main.cpp
@@ -6,7 +6,71 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 15:17:21 by vkatason          #+#    #+#             */
-/*   Updated: 2024/09/17 15:17:26 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/09/19 15:08:00 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> vec;
+    std::list<int> lst;
+    int random;
+    int position;
+
+    std::cout << YELLOW << "Input number of element in vector: " << RST;
+    int v;
+    std::cin >> v;
+    
+    std::cout << YELLOW << "Input number of elements in list: " << RST;
+    int l;
+    std::cin >> l;
+
+    std::cout << YELLOW << "Vector: " << RST;   
+    for (int i = 0; i < v; i++)
+    {
+        random = rand() % 100;
+        vec.push_back(random);
+        std::cout << random << " ";
+    }
+
+    std::cout << std::endl << YELLOW << "List: " << RST;
+    for (int i = 0; i < l; i++)
+    {
+        random = rand() % 100;
+        lst.push_back(random);
+        std::cout << random << " ";
+    }
+
+    std::cout << std::endl << YELLOW << "Enter a number to find in vector: " << RST;
+    int toFind;
+    std::cin >> toFind;
+    
+    try
+    {
+        std::vector<int>::iterator it_vector = easyfind(vec, toFind);
+        std::cout << GREEN << "Element found: " << *it_vector << RST;
+        position = std::distance(vec.begin(), it_vector) + 1;
+        std::cout << GREEN << " Position in vector: " << position << RST << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << RED << e.what() << " in vector" << RST << std::endl;
+    }
+
+    std::cout << YELLOW << "Enter a number to find in list: " << RST;
+    std::cin >> toFind;
+    try
+    {
+       std::list<int>::iterator it_list = easyfind(lst, toFind);
+       std::cout << GREEN << "Element found :" << *it_list << RST;
+       position = std::distance(lst.begin(), it_list) + 1;
+       std::cout << GREEN << " Position in list: " << position << RST << std::endl;      
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RED << e.what() << " in list" << RST << '\n';
+    }
+    return 0;
+}
