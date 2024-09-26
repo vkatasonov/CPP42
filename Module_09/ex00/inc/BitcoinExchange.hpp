f3e6e22d5d7b71d43b353d00697defbcea303c5f
Vladimir Katasonov <vkatason@student.42malaga.com>
2024-09-26 16:45:19 +0200

    Module_09 created

---
+++ Module_09/ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 10:37:53 by vkatason          #+#    #+#             */
+/*   Updated: 2024/09/26 10:52:37 by vkatason         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <time.h>
+#include <iomanip>
+#include <exception>
